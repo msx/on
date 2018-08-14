58efa9345716bace3ae5f03058df5266a4f137c2
msx <msx.github@yahoo.com>
2018-08-14 14:50:25 +0700

    ...

--- _includes/header.js
+++ _includes/header.js
@@ -1,6 +1,6 @@
 (function() {
-	var input = document.querySelector(".header form input");
-	var button = document.querySelector(".header form button");
+	var input=document.querySelector(".header form input");
+	var button=document.querySelector(".header form button");
 	var test=document.createElement("input");
 	var nativeSupport=test.placeholder !==void 0;
 	if (!nativeSupport) {
@@ -11,26 +11,26 @@
 				input.value="";
 				input.style.color="#333";
 			}
-		};
+		}
 		input.onblur=function() {
 			if (input.value==="") {
 				input.value="Cari...";
 				input.style.color="#888";
 			}
-		};
+		}
 	}
 	input.onkeyup = function() {
 		button.style.display = (this.value.length) ? "block" : "none";
-	};
+	}
 	button.onclick = function() {
 		button.style.display = "none";
 		input.value = "Cari...";
 		input.focus();
-	};
+	}
 	function reset() {
-		var r = document.querySelector(".header form input").value="";r.focus();
-	};
-
+		var r = document.querySelector(".header form input").value="";
+		r.focus();
+	}
 	var toggle=document.querySelector(".header .more");
 	var checkbox=document.querySelector(".header #more");
 
@@ -38,5 +38,5 @@
 		var target=e.target;
 		if( !checkbox.checked || toggle.contains(target) || (target===checkbox || target===toggle)) return;
 		checkbox.checked=false;
-	};
+	}
 })();
